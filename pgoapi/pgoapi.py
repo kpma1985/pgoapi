20c877e49db47a05e8393e12dc3d10aa1c24364c
Michael Krug <krumi@hrz.tu-chemnitz.de>
2017-07-03 11:19:57 +0200

    improve headers, add locale as parameter

--- pgoapi/pgoapi.py
+++ pgoapi/pgoapi.py
@@ -72,9 +72,9 @@ class PGoApi:
     def set_logger(self, logger=None):
         self.log = logger or logging.getLogger(__name__)
 
-    def set_authentication(self, provider=None, oauth2_refresh_token=None, username=None, password=None, proxy_config=None, user_agent=None, timeout=None):
+    def set_authentication(self, provider=None, oauth2_refresh_token=None, username=None, password=None, proxy_config=None, user_agent=None, timeout=None, locale=None):
         if provider == 'ptc':
-            self._auth_provider = AuthPtc(user_agent=user_agent, timeout=timeout)
+            self._auth_provider = AuthPtc(user_agent=user_agent, timeout=timeout, locale=locale)
         elif provider == 'google':
             self._auth_provider = AuthGoogle()
         elif provider is None:
